c12b6a70a49fbe12616e8d019bbdc4a9193b3b88
trikitrok <trikitrok@gmail.com>
2016-03-08 17:45:29 +0100

    Showing the first roll

--- src/yahtzee/core.clj
+++ src/yahtzee/core.clj
@@ -1,4 +1,13 @@
 (ns yahtzee.core)
 
-(defn yahtzee []
-  (println "Category: Ones"))
+(defn yahtzee [rand-int]
+  (println "Category: Ones")
+  (println
+    (str "Dice: "
+         (clojure.string/join
+           " "
+           (map #(str "D" % ":" (rand-int 0 6))
+                (range 1 6))))))
+
+(defn make-yahtzee [rand-int]
+  #(yahtzee rand-int))
--- test/yahtzee/core_test.clj
+++ test/yahtzee/core_test.clj
@@ -2,9 +2,19 @@
   (:require [midje.sweet :refer :all]
             [yahtzee.core :refer :all]))
 
+(def stubbed-rands (atom [2 4 1 6 1]))
+
+(defn stub-rand [_ _]
+  (let [r (first @stubbed-rands)]
+    (swap! stubbed-rands rest)
+    r))
+
 (facts
   "about Yahtzee"
-  (fact
-    "it outputs to the console"
-    (let [cout (with-out-str (yahtzee))]
-      (= cout "Category: Ones\n") => true)))
+  (let [yahtzee (make-yahtzee stub-rand)]
+    (fact
+      "it outputs to the console"
+      (let [cout (with-out-str (yahtzee))]
+        (clojure.string/split
+          cout #"\n") => ["Category: Ones"
+                          "Dice: D1:2 D2:4 D3:1 D4:6 D5:1"]))))
