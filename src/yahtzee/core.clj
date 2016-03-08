a8d21bc6128cf83917ca75e0f60b20f2d2c10add
trikitrok <trikitrok@gmail.com>
2016-03-08 19:04:48 +0100

    Keeping state for the rolled dice

--- src/yahtzee/core.clj
+++ src/yahtzee/core.clj
@@ -1,33 +1,41 @@
 (ns yahtzee.core)
 
-(def dice-nums (range 1 6))
+(def dice [:d1 :d2 :d3 :d4 :d5])
 
 (def titles-by-category
   {:ones "Category: Ones"})
 
-(defn roll-dice [roll]
-  (repeatedly 5 roll))
+(def rolled-dice
+  (atom {:d1 nil :d2 nil :d3 nil :d4 nil :d5 nil}))
 
-(defn dice-output-str [roll-dice]
+(def description-by-dice
+  {:d1 "D1", :d2 "D2", :d3 "D3", :d4 "D4", :d5 "D5"})
+
+(defn roll-dice [roll dice]
+  (swap! rolled-dice
+         merge
+         (into {} (for [d dice] [d (roll)]))))
+
+(defn dice-output-str [rolled-dice]
   (str
     "Dice: "
     (clojure.string/join
       " "
-      (map #(str "D" %2 ":" %1)
-           (roll-dice)
-           dice-nums))))
+      (map #(str (description-by-dice %) ":" (rolled-dice %)) dice))))
 
 (defn dice-to-rerun [roll-num]
   (str "[" roll-num "] Dice to re-run:"))
 
-(defn category-output-strs [category roll-num roll-dice]
-  (clojure.string/join "\n"
+(defn produce-category-output [category roll-num]
+  (clojure.string/join
+    "\n"
     [(titles-by-category category)
-     (dice-output-str roll-dice)
+     (dice-output-str @rolled-dice)
      (dice-to-rerun roll-num)]))
 
-(defn yahtzee [roll-dices]
-  (println (category-output-strs :ones 1 roll-dices)))
+(defn yahtzee [roll-dice]
+  (roll-dice dice)
+  (println (produce-category-output :ones 1)))
 
 (defn make-yahtzee [roll]
-  (partial yahtzee #(roll-dice roll)))
+  (partial yahtzee (partial roll-dice roll)))
