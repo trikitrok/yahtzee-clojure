cc96a4e7e8ee522a19f238eb26f6262036782b57
trikitrok <trikitrok@gmail.com>
2016-03-08 17:53:43 +0100

    Extracted rolling dices

--- src/yahtzee/core.clj
+++ src/yahtzee/core.clj
@@ -1,14 +1,18 @@
 (ns yahtzee.core)
 
-(defn yahtzee [roll]
+(defn roll-dices [roll]
+  (repeatedly 5 roll))
+
+(defn yahtzee [roll-dices]
   (println "Category: Ones")
   (println
     (str "Dice: "
          (clojure.string/join
            " "
-           (map #(str "D" % ":" (roll))
+           (map #(str "D" %2 ":" %1)
+                (roll-dices)
                 (range 1 6)))))
   (println "[1] Dice to re-run:"))
 
 (defn make-yahtzee [roll]
-  #(yahtzee roll))
+  (partial yahtzee #(roll-dices roll)))
