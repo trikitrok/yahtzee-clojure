bc99041e3a6abc8b6d727a81d0034f23075f8a0a
trikitrok <trikitrok@gmail.com>
2016-03-08 18:02:13 +0100

    Separated print from output generation

--- src/yahtzee/core.clj
+++ src/yahtzee/core.clj
@@ -5,16 +5,20 @@
 (defn roll-dices [roll]
   (repeatedly 5 roll))
 
+(defn category-output-strs [roll-dices]
+  (clojure.string/join
+    "\n"
+    ["Category: Ones"
+     (str "Dice: "
+          (clojure.string/join
+            " "
+            (map #(str "D" %2 ":" %1)
+                 (roll-dices)
+                 dice-nums)))
+     "[1] Dice to re-run:"]))
+
 (defn yahtzee [roll-dices]
-  (println "Category: Ones")
-  (println
-    (str "Dice: "
-         (clojure.string/join
-           " "
-           (map #(str "D" %2 ":" %1)
-                (roll-dices)
-                dice-nums))))
-  (println "[1] Dice to re-run:"))
+  (println (category-output-strs roll-dices)))
 
 (defn make-yahtzee [roll]
   (partial yahtzee #(roll-dices roll)))
